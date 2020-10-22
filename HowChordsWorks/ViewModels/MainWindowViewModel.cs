dd966a9b6de3cdb18a60b23a8f294fbc0e110f2f
Sébastien Geiser <sgeiser@precitrame.com>
2020-10-22 08:43:53 +0200

    Add project files.

---
+++ HowChordsWorks/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HowChordsWorks.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
