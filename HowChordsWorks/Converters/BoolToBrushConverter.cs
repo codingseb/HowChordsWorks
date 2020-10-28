708b4e4a4f6e4fed32511d416050fd8663839d1d
Sébastien Geiser <sgeiser@precitrame.com>
2020-10-28 14:46:45 +0100

    first tests for sectors for chords wheel

---
+++ HowChordsWorks/Converters/BoolToBrushConverter.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Globalization;
+
+namespace HowChordsWorks.Converters
+{
+    /// <summary>
+    /// -- Describe here to what is this class used for. (What is it's purpose) --
+    /// </summary>
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool bValue && bValue ? Brushes.White : Brushes.AliceBlue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- HowChordsWorks/Views/PiePiece.cs
+++ HowChordsWorks/Views/PiePiece.cs
@@ -18,15 +18,6 @@ namespace HowChordsWorks.Views
             set { SetValue(RadiusProperty, value); }
         }
 
-        public static readonly StyledProperty<double> PushOutProperty =
-            AvaloniaProperty.Register<PiePiece, double>(nameof(PushOut));
-
-        public double PushOut
-        {
-            get { return GetValue(PushOutProperty); }
-            set { SetValue(PushOutProperty, value); }
-        }
-
         public static readonly StyledProperty<double> InnerRadiusProperty =
             AvaloniaProperty.Register<PiePiece, double>(nameof(InnerRadius));
 
@@ -35,61 +26,6 @@ namespace HowChordsWorks.Views
             get { return GetValue(InnerRadiusProperty); }
             set { SetValue(InnerRadiusProperty, value); }
         }
-
-        public static readonly StyledProperty<double> WedgeAngleProperty =
-            AvaloniaProperty.Register<PiePiece, double>(nameof(WedgeAngle));
-
-        public double WedgeAngle
-        {
-            get { return GetValue(WedgeAngleProperty); }
-            set { SetValue(WedgeAngleProperty, value); }
-        }
-
-        public static readonly StyledProperty<double> RotationAngleProperty =
-            AvaloniaProperty.Register<PiePiece, double>(nameof(RotationAngle));
-
-        public double RotationAngle
-        {
-            get { return GetValue(RotationAngleProperty); }
-            set { SetValue(RotationAngleProperty, value); }
-        }
-
-        public static readonly StyledProperty<double> CenterXProperty =
-            AvaloniaProperty.Register<PiePiece, double>(nameof(CenterX));
-
-        public double CenterX
-        {
-            get { return GetValue(CenterXProperty); }
-            set { SetValue(CenterXProperty, value); }
-        }
-
-        public static readonly StyledProperty<double> CenterYProperty =
-            AvaloniaProperty.Register<PiePiece, double>(nameof(CenterY));
-
-        public double CenterY
-        {
-            get { return GetValue(CenterYProperty); }
-            set { SetValue(CenterYProperty, value); }
-        }
-
-        public static readonly StyledProperty<double> PercentageProperty =
-            AvaloniaProperty.Register<PiePiece, double>(nameof(Percentage));
-
-        public double Percentage
-        {
-            get { return GetValue(PercentageProperty); }
-            set { SetValue(PercentageProperty, value); }
-        }
-
-        public static readonly StyledProperty<double> PieceValueProperty =
-            AvaloniaProperty.Register<PiePiece, double>(nameof(PieceValue));
-
-        public double PieceValue
-        {
-            get { return GetValue(PieceValueProperty); }
-            set { SetValue(PieceValueProperty, value); }
-        }
-
         #endregion
 
         protected override Geometry CreateDefiningGeometry()
@@ -110,56 +46,36 @@ namespace HowChordsWorks.Views
         /// </summary>
         private void DrawGeometry(StreamGeometryContext context)
         {
-            Point startPoint = new Point(CenterX, CenterY);
+            double outerWidth = (Radius * Math.Sin(ConvertToRadians(15)));
+            double innerWidth = (InnerRadius * Math.Sin(ConvertToRadians(15)));
+            double innerLeft = outerWidth - innerWidth;
+            double innerRight = outerWidth + innerWidth;
+            double outerH = (Radius * Math.Cos(ConvertToRadians(15)));
+            double beginY =  Radius - outerH + StrokeThickness;
+            double h = outerH - (InnerRadius * Math.Cos(ConvertToRadians(15))) + beginY;
 
-            Point innerArcStartPoint = ToCartesianPoint(RotationAngle, InnerRadius);
-
-            innerArcStartPoint = new Point(innerArcStartPoint.X + CenterX, innerArcStartPoint.Y + CenterY);
-
-            Point innerArcEndPoint = ToCartesianPoint(RotationAngle + WedgeAngle, InnerRadius);
-            innerArcEndPoint = new Point(innerArcEndPoint.X + CenterX, innerArcEndPoint.Y + CenterY);
-
-            Point outerArcStartPoint = ToCartesianPoint(RotationAngle, Radius);
-            outerArcStartPoint = new Point(outerArcStartPoint.X + CenterX, outerArcStartPoint.Y + CenterY);
-
-            Point outerArcEndPoint = ToCartesianPoint(RotationAngle + WedgeAngle, Radius);
-            outerArcEndPoint = new Point(outerArcStartPoint.X + CenterX, outerArcEndPoint.Y + CenterY);
-
-            bool largeArc = WedgeAngle > 180.0;
-
-            if (PushOut > 0)
-            {
-                Point offset = ToCartesianPoint(RotationAngle + (WedgeAngle / 2), PushOut);
-                innerArcStartPoint = new Point(innerArcStartPoint.X + offset.X, innerArcStartPoint.Y + offset.Y);
-                innerArcEndPoint = new Point(innerArcEndPoint.X + offset.X, innerArcEndPoint.Y + offset.Y);
-                outerArcStartPoint = new Point(outerArcStartPoint.X + offset.X, outerArcStartPoint.Y + offset.Y);
-                outerArcEndPoint = new Point(outerArcEndPoint.X + offset.X, outerArcEndPoint.Y + offset.Y);
-            }
-
-            Size outerArcSize = new Size(Radius, Radius);
-            Size innerArcSize = new Size(InnerRadius, InnerRadius);
-            context.BeginFigure(new Point(0, 10), true);
-            context.ArcTo(new Point(50, 10), new Size(50, 50), 0, false, SweepDirection.Clockwise);
-            context.LineTo(new Point(40, 50));
-            context.ArcTo(new Point(10, 50), new Size(50, 50), 0, false, SweepDirection.CounterClockwise);
+            context.BeginFigure(new Point(0, beginY), true);
+            context.ArcTo(new Point(outerWidth * 2, beginY), new Size(Radius, Radius), 0, false, SweepDirection.Clockwise);
+            context.LineTo(new Point(innerRight, h));
+            context.ArcTo(new Point(innerLeft, h), new Size(InnerRadius, InnerRadius), 0, false, SweepDirection.CounterClockwise);
 
             context.EndFigure(true);
-            //context.ArcTo(outerArcEndPoint, outerArcSize, 0, largeArc, SweepDirection.Clockwise);
-            //context.LineTo(outerArcStartPoint);
-            
-            //context.LineTo(innerArcEndPoint);
-            //context.ArcTo(innerArcStartPoint, innerArcSize, 0, largeArc, SweepDirection.Clockwise);
         }
 
         private static Point ToCartesianPoint(double angle, double radius)
         {
             // convert to radians
-            double angleRad = Math.PI / 180.0 * ((angle + 360) % 360);
+            double angleRad = ConvertToRadians((angle + 360) % 360);
 
             double x = radius * Math.Cos(angleRad);
             double y = radius * Math.Sin(angleRad);
 
             return new Point(x, y);
+        }
+
+        private static double ConvertToRadians(double angle)
+        {
+            return Math.PI / 180 * angle;
         }
     }
 }
